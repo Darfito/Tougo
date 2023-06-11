beca3e3446bf6caa7c9be559559156f1dcb058fc
Darfito <darfito.danurdoro-2021@fst.unair.ac.id>
2023-06-11 22:00:47 +0700

    ubah about

--- resources/views/aboutus.blade.php
+++ resources/views/aboutus.blade.php
@@ -1,19 +1,21 @@
 @extends('layouts.main')
 @section('container')
-
-<link rel="stylesheet" href="">
+    <link rel="stylesheet" href="">
     <div class="container">
-      <h1 class="--judul">About Us</h1>
-      <br />
-      <p class="--konten">
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugit ex
-        reiciendis praesentium exercitationem ipsa. Perferendis, rerum.
-        Doloribus accusantium fugiat quo veniam veritatis quae eum quas dolorem
-        atque nihil, nam sequi voluptas dolores aut amet, similique nesciunt,
-        provident dolore quibusdam quos!
-      </p>
+        <h1 class="--judul">About Us</h1>
+        <br />
+        <p class="--konten">
+            Tougo adalah website yang membahas berbagai destinasi wisata pada Pulau Jawa (berfokus pada Jawa Timur, Jawa
+            Tengah, Jawa Barat) dari segi sejarah atau fakta fakta unik didalamnya. Tougo memiliki tujuan untuk membantu
+            masyarakat untuk lebih mengetahui kisah - kisah di balik destinasi wisata yang berada di Pulau Jawa, sehingga
+            apabila nanti masyarakat memiliki kesempatan untuk mengunjungi tempat - tempat tersebut, mereka akan dapat lebih
+            mengingat, mengetahui, serta menghargai segala hal pada destinasi tersebut.
+            <br><br>
+            Warm regards, Tougo's authors, Darfito & Nadhifa
+        </p>
     </div>
-  </body>
-</html>
-{{-- test --}}
+    </body>
+
+    </html>
+    {{-- test --}}
 @endsection
